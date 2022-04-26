70d55e955db592f5f4495e2d73ad464b52c2efa5
jgray20 <jcstofferahn@gmail.com>
2022-04-26 09:43:56 -0500

    Project Updates

--- js/script.js
+++ js/script.js
@@ -18,36 +18,49 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage (list, page) {
-   let startIndex = (page * 9) - 9;
-   let endIndex = page * 9;
-   let studentList = document.querySelector('.student-list');
+   const startIndex = ( page * 9 ) - 9;
+   const endIndex = page * 9;
+   const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
-   for (let i = 0; i < list.length; i++){
-      if (i >= startIndex && i < endIndex) {
+   for ( let i = 0; i < list.length; i++ ) {
+      if ( i >= startIndex && i < endIndex ) {
          const studentItem = document.createElement('li');
-         studentItem.classList.add('student-list', 'cf');
+         studentItem.classList.add('student-item', 'cf')
          studentItem.innerHTML = `
             <div class="student-details">
-               <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
+               <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
                <h3>${list[i].name.first} ${list[i].name.last}</h3>
                <span class="email">${list[i].email}</span>
             </div>
             <div class="joined-details">
                <span class="date">Joined ${list[i].registered.date}</span>
             </div>
-         `; 
-         studentList.insertAdjacentHTML("beforeend", studentItem);
-      }
+         `;
+         studentList.appendChild(studentItem);
+      } 
    }
 }
 showPage(data, 1);
-
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+   const numOfPages = Math.ceil( list.length / 9 )
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
+   for ( let i = 1; i <= numOfPages; i++ ) {
+      let button = document.createElement('li');
+      button.innerHTML = `
+         <button type="button">${i}</button>
+      `;
+      linkList.appendChild(button);
+   }
+   const activeButton = linkList.getElementsByTagName('button')[0];
+   activeButton.className = 'active';
+}
 
-
+addPagination(data);
 
 // Call functions
